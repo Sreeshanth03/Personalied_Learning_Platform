23e8812ddb454a2c99b3d5f20501ea15bb97952c
Sreeshanth03 <sreeshanthsurukutla03@gmail.com>
2025-09-06 12:55:47 +0530

    msg

--- Routers/Lesson_Routes.js
+++ Routers/Lesson_Routes.js
@@ -1,7 +1,19 @@
-const express=require('express');
-const Lesson_Router=express.Router();
-const {createLesson}=require("../Controllers/LessonControllers")
-const {TokenValidators,validateMiddleware}=require("../Validators/authValidators")
-const {checkAuth,checkRole}=require("../Middlewares/authMiddleWare")
-Lesson_Router.post("/",TokenValidators,validateMiddleware,checkAuth,checkRole("Instructor"),createLesson)
-module.exports={Lesson_Router}
+const express = require("express");
+const { upload } = require("../Utiles/Multer");
+const Lesson_Router = express.Router();
+const { createLesson } = require("../Controllers/LessonControllers");
+const {
+  TokenValidators,
+  validateMiddleware,
+} = require("../Validators/authValidators");
+const { checkAuth, checkRole } = require("../Middlewares/authMiddleWare");
+Lesson_Router.post(
+  "/",
+  TokenValidators,
+  validateMiddleware,
+  checkAuth,
+  checkRole("Instructor"),
+  upload.single("content"),
+  createLesson
+);
+module.exports = { Lesson_Router };
