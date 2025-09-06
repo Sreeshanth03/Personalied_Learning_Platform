e7d59f72cefac3c82bb521a32b5aab307cf79ad7
Sreeshanth03 <sreeshanthsurukutla03@gmail.com>
2025-09-06 11:58:47 +0530

    msg

--- Controllers/LessonControllers.js
+++ Controllers/LessonControllers.js
@@ -1,13 +1,13 @@
 const { Lesson_Model } = require("../Models/Lessons");
 async function createLesson(req, res) {
   try {
-    const { content } = req.file;
+    // const { content } = req.file;
     const { title, course, order } = req.body;
     const Lesson_data = new Lesson_Model({
       order: order,
       title: title,
       course: course,
-      content,
+    //   content,
     });
     await Lesson_Model.save();
     res
