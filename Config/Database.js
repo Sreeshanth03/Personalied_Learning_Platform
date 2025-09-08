ffad3958c032b3fa278f264e11bfa2dd3af3a1aa
Sreeshanth03 <sreeshanthsurukutla03@gmail.com>
2025-09-08 10:55:38 +0530

    msg

--- Config/Database.js
+++ Config/Database.js
@@ -15,7 +15,7 @@ dotenv.config();
 async function ConnectionDb() {
   try {
     // ✅ Use template string if you want to include db name separately
-    const uri = `${process.env.MONGO_DB_URI}/${process.env.MONGO_DB_NAME}`;
+    const uri = `${process.env.mongo_db_uri}/${process.env.mongodb_name}`;
 
     await mongoose.connect(uri, {
       useNewUrlParser: true,
